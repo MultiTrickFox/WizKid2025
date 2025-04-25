b0a59dc08f8b2ecdb8ced0259d9689e13f861a15
K <39859227+MultiTrickFox@users.noreply.github.com>
2025-04-25 12:08:01 +0100

    minor pathing error fix

--- backend.js
+++ backend.js
@@ -1,19 +1,12 @@
 function sleep(seconds) { return new Promise(resolve => setTimeout(resolve, seconds * 1000)) }
-
 //
 
-const BASE_URL = 'https://run-fox-run-q28ow.ondigitalocean.app/'
-
-//
-
-
-
-//
+const BASE_URL = 'https://run-fox-run-q28ow.ondigitalocean.app'
 
 async function request_helper(method, endpoint, headers={}, body=null) {
     let result = null
     let xhr = new XMLHttpRequest()
-    xhr.open(method, `${BASE_URL}${endpoint}`, true)
+    xhr.open(method, `${BASE_URL}/${endpoint}`, true)
     xhr.withCredentials = true
     Object.entries(headers).forEach(([key, value]) => { xhr.setRequestHeader(key, value) })
     if (body) { xhr.setRequestHeader('Content-Type', 'application/json') }
@@ -48,7 +41,7 @@ const PASS2 = 'pass456'
 // 1)  GET /view_wizkids
 
 async function ViewWizkids(user='', pass='') {
-    return await request_helper('GET', '/view_wizkids', {user, pass});
+    return await request_helper('GET', 'view_wizkids', {user, pass});
 }
 
 // 2)  PUT /update_wizkid (admin only)
@@ -56,14 +49,14 @@ async function ViewWizkids(user='', pass='') {
 async function UpdateWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser=USER1, updateData={'email':'changed@gmail.com'}) {
     const headers = { 'user': user, 'pass': pass };
     const body = { wizkidUser, ...updateData };
-    return await request_helper('PUT', '/update_wizkid', headers, body);
+    return await request_helper('PUT', 'update_wizkid', headers, body);
 }
 
 // 3)  PUT /update_own_info (wizkid only)
 
-async function UpdateOwnInfo(user=USER2, pass=PASS2, updateData={'name':'MyNameIsChangedToWhite'}) {
+async function UpdateOwnInfo(user=USER2, pass=PASS2, updateData={'name':'CallMeWhite'}) {
     const headers = {'user': user, 'pass': pass };
-    return await request_helper('PUT', '/update_own_info', headers, updateData);
+    return await request_helper('PUT', 'update_own_info', headers, updateData);
 }
 
 // 4)  DELETE /delete_wizkid (admin only)
@@ -71,7 +64,7 @@ async function UpdateOwnInfo(user=USER2, pass=PASS2, updateData={'name':'MyNameI
 async function DeleteWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser='wizkid3') {
     const headers = { 'user': user, 'pass': pass };
     const body = { wizkidUser };
-    return await request_helper('DELETE', '/delete_wizkid', headers, body);
+    return await request_helper('DELETE', 'delete_wizkid', headers, body);
 }
 
 // 5)  POST /fire_wizkid (admin only)
@@ -79,7 +72,7 @@ async function DeleteWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser='wizkid
 async function FireWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser='wizkid3') {
     const headers = { 'user': user, 'pass': pass };
     const body = { wizkidUser };
-    return await request_helper('POST', '/fire_wizkid', headers, body);
+    return await request_helper('POST', 'fire_wizkid', headers, body);
 }
 
 // 6)  POST /unfire_wizkid (admin only)
@@ -87,6 +80,6 @@ async function FireWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser='wizkid3'
 async function UnfireWizkid(user=ADMIN_USER, pass=ADMIN_PASS, wizkidUser='wizkid3') {
     const headers = { 'user': user, 'pass': pass };
     const body = { wizkidUser };
-    return await request_helper('POST', '/unfire_wizkid', headers, body);
+    return await request_helper('POST', 'unfire_wizkid', headers, body);
 }
 
